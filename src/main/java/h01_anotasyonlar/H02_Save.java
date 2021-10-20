a1571edf9594d415e8f6eca397af56df28b0c9b6
Mehmet <m.sozdens@gmail.com>
2021-10-21 01:49:36 +0300

    day1

---
+++ src/main/java/h01_anotasyonlar/H02_Save.java
@@ -0,0 +1,32 @@
+package h01_anotasyonlar;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+//Burada tüm importlar hibernate den yapilacak.
+public class H02_Save {
+	public static void main(String[] args) {
+		
+		//SQL verilerinin oldugu cfg.xml classina baglandik. Bu sayede SQL e baglanmis olduk ve H_Sehir Classina islem yapacagimizi belirtiyoruz;
+		Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H01_Sehir.class);
+		
+		//işlem yapacağımız alanlar için (session) için parent alan oluşturduk;
+		SessionFactory sf = con.buildSessionFactory(); 
+		 
+		
+		//parent alandan kendimize kücük alanlar actik;
+		Session s = sf.openSession(); 
+		
+		//
+		Transaction tx=s.beginTransaction();
+		
+		
+		H01_Sehir sehir1= new H01_Sehir(34, "Istanbul");
+		H01_Sehir sehir2= new H01_Sehir(35, "Izmir");
+		
+		s.save(sehir1);
+		s.save(sehir2);
+		tx.commit();
+		
+	}
+}
